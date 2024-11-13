a38c4ac08b559f7fde2d2d3fd401b4cf01223375
Dafi <dafinmaulana18@gmail.com>
2024-11-14 02:25:08 +0700

    feat: change default page to login

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-  return Inertia::render('Welcome', [
-    'canLogin' => Route::has('login'),
-    'canRegister' => Route::has('register'),
-    'laravelVersion' => Application::VERSION,
-    'phpVersion' => PHP_VERSION,
-  ]);
+  return inertia('Auth/Login');
 });
 
 Route::middleware('auth')->group(function () {
